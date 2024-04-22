fa6a7e22ae5dbcafa023f4c60b5bdc8d7b92d964
revica <revicasamal4520@gmail.com>
2024-04-22 12:03:27 +0200

    hello

---
+++ components/LeaderboardScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export default function LeaderboardScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>leaderboard Screen</Text>
+    </View>
+  );
+}
