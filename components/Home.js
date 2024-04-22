1b187e2efbf74042d33d02cc71cde43b047ba838
revica <revicasamal4520@gmail.com>
2024-04-22 11:42:30 +0200

    hello

---
+++ components/Home.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen ABC</Text>
+    </View>
+  );
+}
